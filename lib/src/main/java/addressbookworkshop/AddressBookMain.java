b55678a94227a1267d404990821a3737a95a66aa
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-31 15:57:51 +0530

    Added: Code to Create a Contact in Address Book

--- lib/src/main/java/addressbookworkshop/AddressBookMain.java
+++ lib/src/main/java/addressbookworkshop/AddressBookMain.java
@@ -2,10 +2,10 @@ package addressbookworkshop;
 
 public class AddressBookMain 
 {
-
-	public static void main(String[] args) 
-	{
-		System.out.println("Welcome to Address Book Program");
-	}
-
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Address Book System");
+        Contacts contact = new Contacts("Nethaji","Ch","Kukatpally","Hyderabad","Telangana",500085,"9454856874","nethaji123@gmail.com");
+        System.out.println(contact);
+    }
 }
